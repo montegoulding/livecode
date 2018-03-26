6d13f08f7a26c1a6105cd956db11810d1d9b85eb
Monte Goulding <monte@appisle.net>
2018-03-26 21:26:22 +1100

    [[ HTTPS ]] Download prebuilts via https
    This patch temporarily adds the `-k` option to the `curl` command
    to skip certificate verification due to an unable to get local
    issuer certificate SSL error.

--- prebuilt/fetch-libraries.sh
+++ prebuilt/fetch-libraries.sh
@@ -20,7 +20,7 @@ SUBPLATFORMS_ios=(iPhoneSimulator6.1 iPhoneSimulator7.1 iPhoneSimulator8.2 iPhon
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 FETCH_DIR="${SCRIPT_DIR}/fetched"
 EXTRACT_DIR="${SCRIPT_DIR}"
-URL="http://downloads.livecode.com/prebuilts"
+URL="https://downloads.livecode.com/prebuilts"
 
 # Versions
 source "${SCRIPT_DIR}/scripts/lib_versions.inc"
@@ -52,9 +52,9 @@ function fetchLibrary {
 		
 		# Download using an HTTP client of some variety
 		if $(which curl 1>/dev/null 2>/dev/null) ; then
-			curl --silent "${URL}/${NAME}.tar.bz2" -o "${FETCH_DIR}/${NAME}.tar.bz2"
+			curl -k "${URL}/${NAME}.tar.bz2" -o "${FETCH_DIR}/${NAME}.tar.bz2"
 		elif $(which wget 1>/dev/null 2>/dev/null) ; then
-			wget "${URL}/${NAME}.tar.bz2" -O "${FETCH_DIR}/${NAME}.tar.bz2"
+			wget --no-check-certificate "${URL}/${NAME}.tar.bz2" -O "${FETCH_DIR}/${NAME}.tar.bz2"
 		else
 			# Perl as a last resort (useful for Cygwin)
 			perl -MLWP::Simple -e "getstore('${URL}/${NAME}.tar.bz2', '${FETCH_DIR}/${NAME}.tar.bz2') == 200 or exit 1"
